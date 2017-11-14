578d15d1c70f3339be00206b6bf424d0717928dc
Martin Müller <martin.mathias.mueller@gmail.com>
2017-11-14 17:43:14 +0100

    fixing loggers

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import redis
 import elasticsearch
+import sys
+from logger import Logger
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -9,14 +11,26 @@ app.config.from_object('config')
 # secret configs
 app.config.from_pyfile('config.py')
 
+logger = Logger.setup('app')
+ 
 # Build connection pool to Redis
 POOL = redis.ConnectionPool(host=app.config['REDIS_HOST'],
         port=app.config['REDIS_PORT'],
         db=app.config['REDIS_DB'])
+redis_conn = redis.Redis(connection_pool=POOL)
+if redis_conn.ping():
+    logger.info('Successfully connected to Redis')
+else:
+    logger.error('Connection to Redis not successful')
 
 # Connect to elasticsearch
 es = elasticsearch.Elasticsearch(["{}:{}".format(app.config['ELASTICSEARCH_HOST'], app.config['ELASTICSEARCH_PORT'])],
         http_auth=(app.config['ELASTICSEARCH_USERNAME'], app.config['ELASTICSEARCH_PASSWORD']))
+if es.ping():
+    logger.info('Successfully connected to ElasticSearch')
+else:
+    logger.error('Connection to ElasticSearch not successful')
+
 
 @app.route('/', methods=['GET'])
 def index():
--- worker.py
+++ worker.py
@@ -2,16 +2,17 @@ from multiprocessing import Pool
 import multiprocessing
 import redis
 import json
-from app import app, POOL
+from app import app, POOL, es
 from process_tweet import ProcessTweet
 import time
 from copy import copy
 import pdb
-import logging
+from logger import Logger
+
 
 def process_tweet(tweet, redis_conn):
     current_process = multiprocessing.current_process()
-    print("Process {} is now processing tweet with id {}".format(current_process.name, tweet['id_str']))
+    logger.debug("Process {} is now processing tweet with id {}".format(current_process.name, tweet['id_str']))
 
     # Strip json
     tweet_stripped = ProcessTweet.strip(copy(tweet))
@@ -34,12 +35,10 @@ def queue_name(name):
     return "{}:{}".format(app.config['REDIS_NAMESPACE'], name)
 
 def submit_tweet(tweet, redis_conn):
-    print('Submit tweet')
-    print(tweet)
+    logger.debug("Submitting tweet with id {} to ES".format(tweet['id']))
 
 def compute_sentiment(tweet, redis_conn):
-    print('Compute sentiment')
-
+    logger.debug("Compute sentiment for tweet with id {} to ES".format(tweet['id']))
 
 def main(parallel=True):
     """main
@@ -47,28 +46,19 @@ def main(parallel=True):
     :param parallel:
     :param with_sleep:
     """
-
-    # set up logging
-    if app.config['DEBUG']:
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.WARNING
-
-    logging.basicConfig(filename=app.config['WORKER_LOGFILE'], level=log_level, format='%(asctime)s %(message)s')
-    logging.info('Start logging...')
-
     # queue names
     logstash_queue = queue_name(app.config['REDIS_LOGSTASH_QUEUE_KEY'])
     submit_queue = queue_name(app.config['REDIS_SUBMIT_QUEUE_KEY'])
     sentiment_queue = queue_name(app.config['REDIS_SENTIMENT_QUEUE_KEY'])
 
-    print("Starting worker pools...")
+    logger.info("Starting worker pools...")
+    logger.debug('Start logging...')
     preprocess_pool = Pool(processes=app.config['NUM_PROCESSES_PREPROCESSING'])
     submit_pool = Pool(processes=app.config['NUM_SUBMIT_PREPROCESSING'])
     sentiment_pool = Pool(processes=app.config['NUM_SENTIMENT_PREPROCESSING'])
 
     while True:
-        print('Fetching new work...')
+        logger.info('Fetching new work...')
         redis_conn = redis.Redis(connection_pool=POOL)
         # pop from queues and assign job to a free worker...
         # _q, _tweet = redis_conn.blpop([logstash_queue, submit_queue, sentiment_queue])
@@ -83,7 +73,7 @@ def main(parallel=True):
             elif q_name == sentiment_queue:
                 res = sentiment_pool.apply_async(compute_sentiment, args=(tweet, redis_conn))
             else:
-                print("Queue name {} is not being processed".format(q_name))
+                logger.warning("Queue name {} is not being processed".format(q_name))
         else:
             if q_name == logstash_queue:
                 process_tweet(tweet, redis_conn)
@@ -92,11 +82,14 @@ def main(parallel=True):
             elif q_name == sentiment_queue:
                 compute_sentiment(tweet, redis_conn)
             else:
-                print("Queue name {} is not being processed".format(q_name))
+                logger.warning("Queue name {} is not being processed".format(q_name))
                 
-            print('That was a lot of work... sleeping for a bit now')
+            logger.info('That was a lot of work... sleeping for a bit now')
             time.sleep(0.2)
 
 
 if __name__ == '__main__':
+    # set up logging
+    logger = Logger.setup('worker', filename='worker.log')
+
     main(parallel=False)
