f81d25622207d23acfcd6ee39a01afa162aaac23
Ubuntu <martin.mathias.mueller@gmail.com>
2017-12-12 14:58:27 +0000

    logstash config file parser

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -42,11 +42,67 @@ def manage_config():
     if request.method == 'GET':
         # load config from file
         with open(path, 'r') as f:
-            data = json.load(f, 'r'))
+            data = json.load(f)
         return jsonify(data)
     else:
         config = request.get_json()
         logger.debug(config)
         with open(path, 'w') as f:
-            data = json.dump(config, f))
+            data = json.dump(config, f)
+
+        parser = TreetopParser(app.config)
+        for d in data:
+            file_data = parser.create_twitter_input(d['keywords'], d['es_index_name'], d['lang'])
+            print(file_data)
+            f_name = d['slug'] + '.conf'
+            path = os.path.join(app.root_path, 'pipeline', 'config', f_name)
+            with open(path, 'w') as f:
+                f.write(file_data)
+
         return Response("Successfully updated", status=200, mimetype='text/plain')
+ 
+
+
+
+
+class TreetopParser():
+    """Parser for logstash config files in treetop format"""
+
+    def __init__(self, config=None):
+        self.config = config
+
+
+    def create_twitter_input(self, keywords, project, lang):
+        data = ""
+        data += treetop_key_start('input')
+        data += treetop_key_start('twitter', nesting_level=1)
+        data += treetop_item('consumer_key', self.config['CONSUMER_KEY'], nesting_level=2)
+        data += treetop_item('consumer_secret', self.config['CONSUMER_SECRET'], nesting_level=2)
+        data += treetop_item('oauth_token', self.config['OAUTH_TOKEN'], nesting_level=2)
+        data += treetop_item('oauth_token_secret', self.config['OAUTH_TOKEN_SECRET'], nesting_level=2)
+        data += treetop_item('keywords', keywords, nesting_level=2)
+        data += treetop_item('languages', lang, nesting_level=2)
+        data += treetop_item('full_tweet', 'true', nesting_level=2, no_quotes=True)
+        data += treetop_item('ignore_retweets', 'true', nesting_level=2, no_quotes=True)
+        data += treetop_item('tags', [project, 'en'], nesting_level=2)
+        data += treetop_key_end(nesting_level=1)
+        data += treetop_key_end(nesting_level=0)
+        return data
+
+    def treetop_key_start(self, key, nesting_level=0):
+        indent = '  '*nesting_level
+        return "{}{} {}\n".format(indent, key, '{')
+
+    def treetop_key_end(self, nesting_level=0):
+        indent = '  '*nesting_level
+        return "{}{}\n".format(indent, '}')
+
+    def treetop_item(self, key, val, nesting_level=0, no_quotes=False):
+        indent = '  '*nesting_level
+        if isinstance(val, str) and not no_quotes:
+            return '{}{} => "{}"\n'.format(indent, key, val)
+        else:
+            return '{}{} => {}\n'.format(indent, key, val)
+
+
+        
