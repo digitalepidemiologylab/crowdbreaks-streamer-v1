937da992b97f7b72f5c6ea968a5a2cd5e29ffa0e
Martin Müller <martin.mathias.mueller@gmail.com>
2020-03-10 22:04:51 +0100

    improve syncing script

--- scripts/sync_es_data.py
+++ scripts/sync_es_data.py
@@ -8,22 +8,18 @@ import os
 from elasticsearch import helpers
 from datetime import datetime
 import logging
+import argparse
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s')
 logger = logging.getLogger(__name__)
 
-# VARS
-INDEX = 'trending_topics_covid'
-QUERY = {"query": {"match_all": {}}}
-DOC_TYPE = '_doc'
-QUERY = {'query': {'range': {'created_at': {'gte': 'now-3d', 'lte': 'now'}}}}
-
-def load_docs(es):
-    scan = helpers.scan(es.es, scroll='1m', query=QUERY, index=INDEX, doc_type=DOC_TYPE)
+def load_docs(es, args):
+    query = {'query': {'range': {'created_at': {'gte': args.since, 'lte': args.until}}}}
+    scan = helpers.scan(es.es, scroll='1m', query=query, index=args.index, doc_type=args.doc_type)
     for item in scan:
-        yield {'_index': INDEX, '_type': DOC_TYPE, '_id': item['_id'], '_source': item['_source']}
+        yield {'_index': args.index, '_type': args.doc_type, '_id': item['_id'], '_source': item['_source']}
 
-def main():
+def main(args):
     es_prd = get_es_client(env='prd')
     if not es_prd.test_connection():
         raise Exception('Connection issue')
@@ -31,13 +27,22 @@ def main():
     # bulk index on development
     es_dev = get_es_client(env='dev')
     try:
-        resp = helpers.bulk(es_dev.es, load_docs(es_prd))
+        resp = helpers.bulk(es_dev.es, load_docs(es_prd, args))
     except Exception as e:
         logger.error('Bulk indexing not unsuccessful!')
         print(e)
     else:
         logger.info(f'Successfully indexed {resp[0]:,} docs...')
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--index', required=True, type=str, help='Name of index')
+    parser.add_argument('-d', '--doc-type', dest='doc_type', default='tweet', required=False, type=str, help='Doc type')
+    parser.add_argument('-s', '--since', default='now-1h', required=False, type=str, help='Since')
+    parser.add_argument('-u', '--until', default='now', required=False, type=str, help='Until')
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
