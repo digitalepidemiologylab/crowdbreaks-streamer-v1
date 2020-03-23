998a8dc26a85848df42f1ad8cf76cf4c546810b0
Martin Müller <martin.mathias.mueller@gmail.com>
2020-03-23 16:26:32 +0100

    remove old scripts, legacy code

--- build_production.sh
+++ build_production.sh
@@ -3,8 +3,5 @@
 # stop any running docker containers...
 sudo docker stop $(sudo docker ps -a -q)
 
-# download binaries...
-bash ./scripts/download_binaries.sh
-
 # run docker compose
 sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
