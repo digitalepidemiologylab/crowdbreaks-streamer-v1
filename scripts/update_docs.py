97808be18b146bddb4b58ddfca8eaecd46548346
Martin Müller <martin.mathias.mueller@gmail.com>
2020-04-01 14:16:06 +0200

    improve update doc script

--- scripts/update_docs.py
+++ scripts/update_docs.py
@@ -2,42 +2,42 @@
 This script bulk updates a field in Elasticsearch
 """
 from elasticsearch import helpers
+from elasticsearch.helpers import BulkIndexError
 import argparse
 import logging
 import pickle
 from utils import get_es_client
 from tqdm import tqdm
+import json
+from collections import defaultdict
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s')
 logger = logging.getLogger(__name__)
 
 def load_data(input_file):
-    with open(input_file, 'rb') as f:
-        actions = pickle.load(f)
-    return actions
+    if input_file.endswith('.pkl'):
+        with open(input_file, 'rb') as f:
+            actions = pickle.load(f)
+    elif input_file.endswith('.jsonl'):
+        num_actions = sum(1 for _ in open(input_file, 'r'))
+        with open(input_file, 'r') as f:
+            for line in tqdm(f, total=num_actions):
+                yield json.loads(line)
 
 def main(args):
     es = get_es_client(env=args.es)
-    logger.info(f'Loading data from {args.input}...')
-    actions = load_data(args.input)
-    num_actions = len(actions)
-    logger.info(f'Loaded {num_actions:,} actions...')
-    logger.info('Start updating docs...')
-    batch_size = 1000
-    for i in tqdm(range(0, num_actions, batch_size)):
-        try:
-            resp = helpers.bulk(es.es, actions[i:(i+batch_size)])
-        except Exception as e:
-            logger.error('Bulk updating not unsuccessful!')
-            print(e)
-        else:
-            logger.info(f'Successfully indexed {resp[0]:,} batches of docs...')
+    logger.info(f'Streaming bulk operations from {args.input}...')
+    resp = helpers.streaming_bulk(es.es, load_data(args.input), chunk_size=1000, max_retries=5, raise_on_error=not args.ignore_errors)
+    for ok, _resp in resp:
+        if not ok:
+            logger.error(_resp)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', required=True, type=str, help='Name of of input file (pickled list of update actions)')
     parser.add_argument('--es', required=True, choices=['dev', 'stg', 'prd'], type=str, help='Which Elasticsearch instance to update docs on')
+    parser.add_argument('--ignore-errors', dest='ignore_errors', action='store_true', help='Ignore errors which happen during indexing')
     args = parser.parse_args()
     return args
 
